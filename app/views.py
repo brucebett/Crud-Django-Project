0a3f1702c135224e7d0e4730da3a8f6c4186b0d4
brucebett <bett6448@gmail.com>
2024-07-17 11:20:30 +0300

    wen update

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from app.models import Students
 
 
 # Create your views here.
 def index(request):
+    data = Students.objects.all()
+    context = {'data': data}
+    return render(request, 'index.html', context)
+
+
+def save(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        form = Students(name=name, email=email, age=age, gender=gender)
+        form.save()
+        return redirect("/")
     return render(request, 'index.html')
-
-
-def base(request):
-    return render(request, 'base.html')
